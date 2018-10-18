8e8becd1756bf5d204ffc4ff3b1c18071591c985
alexlhanson <alexlhanson@gmail.com>
2018-10-18 12:01:05 -0700

    Add dependencies for testing

---
+++ src/__test__/src/components/dashboard/index.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Enzyme from 'enzyme';
+import sinon from 'sinon';
+import Adapter from 'enzyme-adapter-react-16';
+import 'jest';
+
+Enzyme.configure({ adapter: new Adapter });
+import Dashboard from '../../../../src/components/dashboard/index';
+
+describe('Dashboard component', () => {
+
+  test('', () => {
+
+  })
+});
