75a7ce769eeb927590edb4d6efe765df5df84d2c
alexlhanson <alexlhanson@gmail.com>
2018-10-18 16:58:12 -0700

    Add tests for noteform

--- src/components/note-create-form/__test__/index.test.js
+++ src/components/note-create-form/__test__/index.test.js
@@ -11,7 +11,7 @@ describe('NoteForm component', () => {
 
   test('should show changes on title input', () => {
     let onChange = sinon.spy(NoteForm.prototype, 'handleChange');
-    let wrapper = Enzyme.shallow('<NoteForm />');
+    let wrapper = Enzyme.shallow(<NoteForm />);
     const event = {target: {
       name: "title",
       value: "Note1"
@@ -20,11 +20,12 @@ describe('NoteForm component', () => {
     wrapper.find('input[name="title"]').simulate('change', event);
 
     expect(onChange.callCount).toBe(1);
+    onChange.restore();
   })
 
   test('should show changes on content input', () => {
     let onChange = sinon.spy(NoteForm.prototype, 'handleChange');
-    let wrapper = Enzyme.shallow('<NoteForm />');
+    let wrapper = Enzyme.shallow(<NoteForm />);
     const event = {target: {
       name: "content",
       value: "Note1"
@@ -33,6 +34,23 @@ describe('NoteForm component', () => {
     wrapper.find('input[name="content"]').simulate('change', event);
 
     expect(onChange.callCount).toBe(1);
+    onChange.restore();
+  })
+
+  test('should show new Note created with form input', () => {
+    let onComplete = sinon.spy(NoteForm.prototype, 'onComplete');
+    let wrapper = Enzyme.shallow(<NoteForm />);
+    wrapper.setProps({ addNote: sinon.fake() })
+    
+    const event = {
+      preventDefault: sinon.fake(),
+      target: {
+        title: "note1",
+        content: "note content"
+      },
+    };
+
+    wrapper.find('form').simulate('submit', event);
   })
 
 });
