bb5f92ab311ffe66408aa4244f3911f259f9f95a
alexlhanson <alexlhanson@gmail.com>
2018-10-21 12:45:52 -0700

    add structure for tests and update to documentation

---
+++ src/components/note-update-form/__test__/index.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Enzyme from 'enzyme';
+import sinon from 'sinon';
+import Adapter from 'enzyme-adapter-react-16';
+import 'jest';
+
+Enzyme.configure({ adapter: new Adapter });
+import NoteUpdateForm from '../index';
+
+describe('NoteUpdateForm', () => {
+
+  test('should show NoteUpdateForm ability to update a form in App state', () => {
+
+  })
+});
